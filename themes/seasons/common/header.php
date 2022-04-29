56db76fec74323566f0d857fb2abcbe20b190e37
Mark Miles <mmiles2@gmail.com>
2022-04-29 09:24:51 -0500

    css

--- themes/seasons/common/header.php
+++ themes/seasons/common/header.php
@@ -56,10 +56,11 @@
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
             <div id="site-title">
                 <?php echo link_to_home_page(theme_logo()); ?>
-            </div>
-            <div id="site-title-2">
                 <img src="/files/theme_uploads/stories-logo-white.png">
             </div>
+            <!-- <div id="site-title-2">
+                <img src="/files/theme_uploads/stories-logo-white.png">
+            </div> -->
             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
                 <?php echo search_form(array('show_advanced' => true)); ?>
