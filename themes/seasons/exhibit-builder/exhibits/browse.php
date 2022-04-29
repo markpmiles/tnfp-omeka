e13fc8aeb3883e866aa3e2fc34063f66354f2895
Mark Miles <mmiles2@gmail.com>
2022-04-29 11:15:20 -0500

    css

--- themes/seasons/exhibit-builder/exhibits/browse.php
+++ themes/seasons/exhibit-builder/exhibits/browse.php
@@ -2,7 +2,7 @@
 $title = __('Browse Exhibits');
 echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
 ?>
-<h1><?php echo $title; ?> <?php echo __('(%s total)', $total_results); ?></h1>
+<!-- <h1><?php echo $title; ?> <?php echo __('(%s total)', $total_results); ?></h1> -->
 <?php if (count($exhibits) > 0): ?>
 
 <nav class="navigation" id="secondary-nav">
