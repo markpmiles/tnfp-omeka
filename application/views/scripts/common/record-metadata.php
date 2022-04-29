da16252991d3bff0b20d8e3c4b52019c0f032896
Mark Miles <mmiles2@gmail.com>
2022-04-29 11:09:25 -0500

    css

--- application/views/scripts/common/record-metadata.php
+++ application/views/scripts/common/record-metadata.php
@@ -5,7 +5,7 @@
     <?php endif; ?>
     <?php foreach ($setElements as $elementName => $elementInfo): ?>
     <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-        <h3><?php echo html_escape(__($elementName)); ?></h3>
+        <!-- <h3><?php echo html_escape(__($elementName)); ?></h3> -->
         <?php foreach ($elementInfo['texts'] as $text): ?>
             <div class="element-text"><?php echo $text; ?></div>
         <?php endforeach; ?>
